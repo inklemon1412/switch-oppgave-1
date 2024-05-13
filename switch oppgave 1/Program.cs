e5fb238d2a677428800bd5810b4a06cc5dd86b2a
inklemon1412 <jonasnie@getacademy.no>
2024-05-13 14:21:28 +0200

    Add project files.

---
+++ switch oppgave 1/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace switch_oppgave_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = 2;
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Today is Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Today is Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Today is Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Today is Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Today is Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Today is Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Today is Sunday");
+                    break;
+            }
+        }
+    }
+}
+
+                        
